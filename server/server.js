b4e0e54b1ee49c055ee3f310d0813a9b80ed4b1a
iuriseara <iuriseara@gmail.com>
2021-06-18 13:27:14 -0400

    Mongoose Package update & Mongo DB Setup

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb+srv://iuri:iuri@cluster0.eseck.mongodb.net/Data?retryWrites=true&w=majority';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
