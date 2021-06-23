e96fb4a83bd0aa232e9b6a9553ccd3ca6e5ddc83
iuriseara <iuriseara@gmail.com>
2021-06-23 15:28:35 -0400

    Learned Nested Routing

---
+++ client/components/LyricList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+
+class LyricList extends Component {
+    render() {
+        return (
+            <ul>
+                
+            </ul>
+        );
+    }
+}
+
+export default LyricList;
--- client/index.js
+++ client/index.js
@@ -6,6 +6,7 @@ import { ApolloProvider } from 'react-apollo';
 import SongList from './components/SongList';
 import App from './components/App'
 import SongCreate from './components/SongCreate'
+import SongDetail from './components/SongDetail'
 import './style/style.css'
 
 const client = new ApolloClient({});
@@ -16,10 +17,8 @@ const Root = () => {
         <Router history={hashHistory}>
           <Route path="/" component={App}>
               <IndexRoute component={SongList}/>
-          </Route>
-
-          <Route path="/songs/new" component={SongCreate}>
-
+              <Route path="/songs/new" component={SongCreate} />
+              <Route path="/songs/:id" component={SongDetail} />
           </Route>
         </Router>
     </ApolloProvider>
